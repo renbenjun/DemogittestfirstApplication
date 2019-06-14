0aca2b722ad3b4dc3c857abeb7046516d9d3d3e4
renbenjun <2385075928@qq.com>
2019-06-14 17:54:47 +0800

    first commit

---
+++ src/main/java/com/example/demogittestfirst/DemogittestfirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogittestfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogittestfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogittestfirstApplication.class, args);
+    }
+
+}
